d6ba69bdaeca26f90c47f454a7bf5258c56be223
thisisRaniMJ <ranimjiddi278@gmail.com>
2024-02-23 00:41:38 +0530

    TuneHub Application

--- TuneHubApp/src/main/java/com/example/tunehub/repositories/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tunehub.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tunehub.entities.Playlist;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Integer>{
-
-}
